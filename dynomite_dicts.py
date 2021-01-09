feff07604f6247b3b98de39f6c53ea4ca8b51382
Steph Leif <stephleif@gmail.com>
2021-01-09 14:56:17 -0800

    completed

---
+++ dynomite_dicts.py
@@ -0,0 +1,22 @@
+#dynomite_dicts
+"""
+in the third file, called dynomite_dicts.py do the following:
+
+Create an empty dictionary called pokedex.
+Add the following key, value pairs to the dictionary:
+  'Venosaur': ['Grass', 'Poisen']
+  'Charizard': ['Fire', 'Flying']
+  'Blastoise': ['Water']
+
+ 
+
+Remove 'Blastoise' from the dictionary.
+"""
+if __name__ == "__main__":
+    pokedex = dict()
+    #pokemon_types['Squirtle'] = 'water'
+    pokedex['Venosaur'] = ['Grass', 'Poisen']
+    pokedex['Charizard'] = ['Fire', 'Flying']
+    pokedex['Blastoise'] = ['Water']
+    #del pokemon_types['squirtel']
+    del pokedex['Blastoise']
